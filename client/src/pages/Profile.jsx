13e151bab5390a7ada344876a46c4f27025c3570
Ehtisham <ehtishamzahid039@gmail.com>
2024-04-20 00:32:55 +0500

    Changes to be committed: 	modified:   api/controllers/listing.controller.js 	modified:   api/routes/listing.route.js 	modified:   client/src/App.jsx 	modified:   client/src/pages/Profile.jsx 	new file:   client/src/pages/UpdateListing.jsx

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -227,7 +227,9 @@ export default function Profile() {
 
               <div className="flex flex-col items-center">
                 <button onClick={() => handelListingDelete(listing._id)} className='text-red-700'>Delete</button>
-                <button className='text-green-700'>Edit</button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className='text-green-700'>Edit</button>
+                </Link>
               </div>
             </div>
           ))}
