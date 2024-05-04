448dd7caac616cb4efe9d4029cbc2fd8a8e167ec
Ehtisham Zahid <ehtishamzahid039@gmail.com>
2024-05-04 15:55:36 +0500

    Created the listing item component and show the listing

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -4,7 +4,7 @@ import { MdLocationOn } from 'react-icons/md'
 export default function ListingItem({ listing }) {
     return (
         <div className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[320px]'>
-            <Link>
+            <Link to={`/listing/${listing._id}`}>
                 <img src={listing.imageUrls[0]} alt="listing image" className='h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300' />
                 <div className="p-3 flex flex-col gap-2 w-full">
                     <p className='truncate text-lg font-semibold text-slate-700'>{listing.name}</p>
@@ -15,12 +15,18 @@ export default function ListingItem({ listing }) {
                     <p className='text-sm text-grey-600 line-clamp-2'>
                         {listing.description}</p>
 
-                    <p className='text-slate-500 mt-2 font-semibold'>
-                        $ {listing.offer === true ? listing.discountPrice.toLocaleString() : listing.regularPrice.toLocaleString()}
-
+                    <p className='text-slate-500 mt-2 font-semibold '>
+                        $ {listing.offer ? listing.discountPrice : listing.regularPrice}
                         {listing.type === 'rent' && '/month'}
                     </p>
-
+                    <div className="text-slate-700 flex gap-4">
+                        <div className="font-bold text-xs">
+                            {listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : `${listing.bedrooms} Bed`}
+                        </div>
+                        <div className="font-bold text-xs">
+                            {listing.bathrooms > 1 ? `${listing.bathrooms} Baths` : `${listing.bathrooms} bath`}
+                        </div>
+                    </div>
                 </div>
             </Link>
         </div>
